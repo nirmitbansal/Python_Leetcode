b081ab8db38641f81c365edef72fa8d8ee97db13
NIRMIT BANSAL <bansalnirmit3@gmail.com>
2026-04-18 11:28:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (40.62%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        
+        for i in range(len(strs[0])):
+            for word in strs[1:]:
+                if i == len(word) or word[i] != strs[0][i]:
+                    return strs[0][:i]
+        
+        return strs[0]
